db47f2f93296880b06ea40727c8ab12986714a79
Caique <caique.c.s@outlook.com>
2020-11-01 23:20:43 -0300

    calcula desconto e deleta pedidos.

--- src/main/java/br/com/caique/desafiozup/controller/PedidosController.java
+++ src/main/java/br/com/caique/desafiozup/controller/PedidosController.java
@@ -1,16 +1,20 @@
 package br.com.caique.desafiozup.controller;
 
+import br.com.caique.desafiozup.dto.PedidoDto;
 import br.com.caique.desafiozup.form.PedidoForm;
-import br.com.caique.desafiozup.form.ProdutoForm;
 import br.com.caique.desafiozup.model.Pedido;
-import br.com.caique.desafiozup.model.Produto;
 import br.com.caique.desafiozup.repository.PedidoRepository;
 import br.com.caique.desafiozup.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pedido")
@@ -23,9 +27,9 @@ public class PedidosController {
     PedidoRepository pedidoRepository;
 
     @GetMapping
-    public List<Pedido> listar() {
-        List<Pedido> pedido = pedidoRepository.findAll();
-        return pedido;
+    public ResponseEntity<Page<PedidoDto>> listar() {
+        Pageable pageable = PageRequest.of(0, 10);
+        return ResponseEntity.ok(new PedidoDto().converter(pedidoRepository.findAll(pageable)));
     }
 
     @PostMapping
@@ -33,14 +37,26 @@ public class PedidosController {
     public PedidoForm cadastrar(@RequestBody PedidoForm form) {
         Pedido pedido = form.converter(produtoRepository);
         pedidoRepository.save(pedido);
+        //TODO retornar responseEntity de um DTO
         return form;
     }
 
-    public void remover() {
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        Optional<Pedido> pedidoOptional = pedidoRepository.findById(id);
 
+        if(pedidoOptional.isPresent()) {
+            pedidoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
-    public void atualizar() {
-
+    @PutMapping("/{id}")
+    @Transactional
+    public void atualizar(@PathVariable Long id) {
+        //TODO: PedidoAtualizacaoForm form
     }
 }
